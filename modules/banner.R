9ab4701d610e074a7c1918619167212af5c115c0
cyyllam <cyyllam@gmail.com>
2024-06-05 10:58:04 -0700

    resize image

--- modules/banner.R
+++ modules/banner.R
@@ -4,21 +4,26 @@ banner_ui <- function(id, photo_filename, banner_title, banner_subtitle = NULL) 
   ns <- NS(id)
   
   tagList( 
-    fluidRow(column(4, style = 'padding-right:0px;',
-                    div(
-                      img(src = photo_filename, 
-                          width = "100%", 
-                          style = "padding-top: 0px; border-radius:0 0 30px 0;")
-                    )
-    ),
-    column(8, style = 'padding-left:0px;',
-           jumbotron(
-             title = strong(div(class="mainpage_title", banner_title)),
-             status = "success",
-             btnName = strong(div(class="mainpage_subtitle", banner_subtitle))
-           )
-    )
-    )
+    fluidRow(
+      column(4, 
+             style = 'padding-right:0px;',
+             div(class = "banner",
+                 img(src = photo_filename, 
+                     width = "100%",
+                     height = "280px",
+                     style = "padding-top: 0px; border-radius:0 0 50px 0;"
+                     )
+             )
+      ),
+      column(8, 
+             style = 'padding-left:0px;',
+             jumbotron(
+               title = strong(div(class="mainpage_title", banner_title)),
+               status = "success",
+               btnName = strong(div(class="mainpage_subtitle", banner_subtitle))
+             )
+      )
+    ) # end fluid row
   )
   
 }
