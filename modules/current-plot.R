bfd5d8baa21769d43ed886d85df8b1faa7818295
cyyllam <cyyllam@gmail.com>
2024-07-23 14:18:06 -0700

    adjusted plot settings to avoid labels being cutoff

--- modules/current-plot.R
+++ modules/current-plot.R
@@ -20,8 +20,9 @@ current_plot_server <- function(id, go, crosstab_table, var_one, var_two, visopt
       
       div(
         withSpinner(
-          echarts4rOutput(ns('plot'), width = "100%", height = "70rem"),
-          # plotOutput(ns('plot')),
+          div(
+            echarts4rOutput(ns('plot'), width = "100%", height = "50rem")
+          ),
           type = 5,
           color = psrc_colors$pgnobgy_10[sample.int(10, 1)]
         ),
@@ -66,32 +67,35 @@ current_plot_server <- function(id, go, crosstab_table, var_one, var_two, visopt
     })
     
     output$plot <- renderEcharts4r({
-    # output$plot <- renderPlot({
-
-      # static_column_chart(t = clean_table(),
-      #                     x = var_one,
-      #                     y = settings()$p,
-      #                     moe = settings()$m,
-      #                     est = settings()$e,
-      #                     fill = var_two,
-      #                     color = 'pgnobgy_10',
-      #                     source = 'Puget Sound Regional Household Travel Survey')
+      # analyze unique # of x var_labels
+      df <- clean_table()
+      
+      # adjust labels and rotation
+      num_x_labels <- length(unique(df[[var_one]]))
+      
+      if(num_x_labels <= 2) {
+        setting_x_label_str_wrap <- NULL
+        setting_x_label_rotate <- 0
+      } else {
+        setting_x_label_str_wrap <- 10
+        setting_x_label_rotate <- 90
+      }
       
       echart_bar_chart(
-        t = clean_table(),
+        t = df,
         x = var_one,
         y = settings()$p,
         est = settings()$e,
         fill = var_two,
-        # moe = NULL,
+        moe = settings()$m,
         pos = NULL,
         column_vs_bar = "column",
         color = psrc_colors$pgnobgy_10,
         legend_str_wrap = 30,
-        x_label_str_wrap = 10,
-        egrid_left = "5%",
+        x_label_str_wrap = setting_x_label_str_wrap,
+        egrid_left = "10%", 
         egrid_bottom = "20%",
-        x_label_rotate = 90
+        x_label_rotate = setting_x_label_rotate
       )
       
     })
